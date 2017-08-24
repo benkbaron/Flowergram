77dc13e15faeb05f449d995c413c44ec88258b64
Ben <benkbaron@gmail.com>
2017-08-24 14:27:16 -0400

    Add protected route

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,16 +3,16 @@ import ProfileContainer from './profile_container';
 import SessionFormContainer from './session_form_container';
 import { navBar } from './nav_bar';
 import { Route, Redirect, Switch, Link, HashRouter } from 'react-router-dom';
-import { AuthRoute } from '../util/route_util';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
 
 
 const App = () => (
   <div className="app">
 
     <div className="home-auth">
-      <ProfileContainer />
     <switch>
       <AuthRoute path="/" component={SessionFormContainer} />
+      <ProtectedRoute path="/" component={ProfileContainer} />
 
     </switch>
     </div>
--- frontend/util/route_util.jsx
+++ frontend/util/route_util.jsx
@@ -3,6 +3,10 @@ import { connect } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
 import { withRouter } from 'react-router';
 
+const mapStateToProps = state => (
+  {loggedIn: Boolean(state.session.currentUser)}
+);
+
 const Auth = ({component: Component, path, loggedIn}) => (
   <Route path={path} render={(props) => (
     !loggedIn ? (
@@ -13,8 +17,15 @@ const Auth = ({component: Component, path, loggedIn}) => (
   )}/>
 );
 
-const mapStateToProps = state => {
-  return {loggedIn: Boolean(state.session.currentUser)};
-};
+const Protected = ({ component: Component, path, loggedIn }) => (
+  <Route path={path} render={(props) => (
+     loggedIn ? (
+      <Component {...props} />
+    ) : (
+      <Redirect to="/" />
+    )
+  )}/>
+);
 
 export const AuthRoute = withRouter(connect(mapStateToProps, null)(Auth));
+export const ProtectedRoute = withRouter(connect(mapStateToProps, null)(Protected));

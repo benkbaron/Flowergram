833dddcb0ab5e4834439b2e62d708848e3ad780c
Ben <benkbaron@gmail.com>
2017-08-24 20:56:06 -0400

    Add bottom nav

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -5,6 +5,18 @@ import { Route, Redirect, Switch, Link, HashRouter } from 'react-router-dom';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 
 
+const bottomNav = () => (
+  <ul className="bottom-nav">
+    <div className="bottom-nav-section">
+      <li><a className="bottom-nav-links" href="https://www.linkedin.com/in/ben-baron-015381118">LINKED IN</a></li>
+      <li><a className="bottom-nav-links" href="https://github.com/benkbaron">GIT HUB</a></li>
+    </div>
+    <div className="bottom-nav-section">
+      <li className="bottom-nav-links">CREATED BY BEN BARON</li>
+    </div>
+  </ul>
+);
+
 const App = () => (
   <div className="app">
     <div className="home-auth">
@@ -12,6 +24,7 @@ const App = () => (
         <AuthRoute path="/" component={SessionFormContainer} />
         <ProtectedRoute path="/" component={Main} />
       </switch>
+      {bottomNav()}
     </div>
   </div>
 );

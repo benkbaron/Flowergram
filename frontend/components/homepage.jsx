cea534b3355bc73294571b99851994f532a58928
Ben <benkbaron@gmail.com>
2017-08-29 18:00:13 -0400

    Style follow index

--- frontend/components/homepage.jsx
+++ frontend/components/homepage.jsx
@@ -7,6 +7,7 @@ class Homepage extends React.Component {
   constructor(props){
     super(props);
     this.state = { fetching: true };
+    this.handleFollow = this.handleFollow.bind(this);
   }
 
   componentDidMount(){
@@ -14,12 +15,20 @@ class Homepage extends React.Component {
       then(() => this.setState({fetching: false}));
   }
 
+  handleFollow({followee}){
+    return e => {
+      e.preventDefault();
+      this.props.createFollow(followee);
+    };
+  }
+
   followIcon(user){
     return (
-      <ul>
-        <li>{user.username}</li>
-        <li>{user.full_name}</li>
-        <li><img className="follow-pic" src={user.profile_pic}/></li>
+      <ul className="follow-item-contents">
+        <li className="follow-item-data"><img className="follow-pic" src={user.profile_pic}/></li>
+        <li className="follow-item-data username">{user.username}</li>
+        <li className="follow-item-data full-name">{user.full_name}</li>
+        <li className="follow-item-data"><button onClick={this.handleFollow({followee: user})}>Follow</button></li>
       </ul>
     );
   }
@@ -30,7 +39,7 @@ class Homepage extends React.Component {
     });
     return (<ul className="follow-index">
     {users.map((user) =>
-      <li key={user.user.id}>{this.followIcon(user.user)}</li>
+      <li className="follow-user-item" key={user.user.id}>{this.followIcon(user.user)}</li>
     )}
     </ul>);
   }
--- frontend/components/homepage_container.js
+++ frontend/components/homepage_container.js
@@ -2,6 +2,7 @@ import { connect } from 'react-redux';
 import { logout } from '../actions/session_actions';
 import { getAllPosts } from '../actions/post_actions';
 import { makeComment } from '../actions/comment_actions';
+import { createFollow } from '../actions/follow_actions';
 import { fetchAllUsers } from '../actions/user_actions';
 import Homepage from './homepage';
 import { asArray } from '../reducers/selectors';
@@ -17,6 +18,7 @@ const mapDispatchToProps = (dispatch) => ({
   getAllPosts: () => dispatch(getAllPosts()),
   makeComment: (comment) => dispatch(makeComment(comment)),
   fetchAllUsers: () => dispatch(fetchAllUsers()),
+  createFollow: (user) => dispatch(createFollow(user)),
   logout: () => dispatch(logout())
 });
 

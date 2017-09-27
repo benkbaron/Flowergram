6a87c18d1f2d9b733bf48e5fd62a872e14e39f08
Ben <benkbaron@gmail.com>
2017-09-27 16:48:20 -0400

    Update bottom nav links

--- frontend/components/bottomNav.jsx
+++ frontend/components/bottomNav.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 const bottomNav = () => (
   <ul className="bottom-nav">
     <div className="bottom-nav-section">
-      <li><a className="bottom-nav-links" href="https://www.linkedin.com/in/ben-baron-015381118">LINKED IN</a></li>
-      <li><a className="bottom-nav-links" href="https://github.com/benkbaron">GIT HUB</a></li>
+      <li><a className="bottom-nav-links" target="_blank" href="https://www.linkedin.com/in/ben-baron-015381118">LINKED IN</a></li>
+      <li><a className="bottom-nav-links" target="_blank" href="https://github.com/benkbaron">GIT HUB</a></li>
     </div>
     <div className="bottom-nav-section">
-      <li className="bottom-nav-links">CREATED BY BEN BARON</li>
+      <li><a className="bottom-nav-links" target="_blank" href="http://benbaron.info">CREATED BY BEN BARON</a></li>
     </div>
   </ul>
 );

624494b594510d82efba852ec028a3d434660031
Ben <benkbaron@gmail.com>
2017-11-13 17:48:30 -0500

    Update bottom nav with images for links

--- frontend/components/navbar/bottomNav.jsx
+++ frontend/components/navbar/bottomNav.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const bottomNav = () => (
   <ul className="bottom-nav">
-    <div className="bottom-nav-section">
-      <li><a className="bottom-nav-links" target="_blank" href="https://www.linkedin.com/in/ben-baron-015381118">LINKED IN</a></li>
-      <li><a className="bottom-nav-links" target="_blank" href="https://github.com/benkbaron">GIT HUB</a></li>
+    <div className="bottom-nav-section" className="bottom-nav-section-left">
+      <li><a target="_blank" href="https://www.linkedin.com/in/ben-baron-015381118"><img className="bottom-nav-links" src={`${window.images.linkedinIcon}`}/></a></li>
+      <li><a target="_blank" href="https://github.com/benkbaron"><img className="bottom-nav-links" src={`${window.images.githubIcon}`}/></a></li>
     </div>
     <div className="bottom-nav-section">
       <li><a className="bottom-nav-links" target="_blank" href="http://benbaron.info">CREATED BY BEN BARON</a></li>
